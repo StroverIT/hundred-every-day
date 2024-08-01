86983f7b2a6660158de2a9af00f37951d57962da
StroverIT <StroverIT>
2024-08-01 15:28:54 +0300

    Initial

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,16 +1,15 @@
 import { getServerSession } from "next-auth";
 import options from "@/api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
-export const dynamic = "force-dynamic"
+import Index from "@/components/pages/account/Index";
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const session = await getServerSession(options);
 
-  if(!session){
-    redirect("/")
+  if (!session) {
+    redirect("/");
   }
-  
-  return (
-    <div>Testvam brat</div>
-  )
+
+  return <Index />;
 }
