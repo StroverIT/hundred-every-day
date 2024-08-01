420a5d832d78a770f5e83b1b2c309df0e6037950
StroverIT <StroverIT>
2024-08-02 00:45:26 +0300

    Adding from previous days

--- API/server/training.ts
+++ API/server/training.ts
@@ -28,12 +28,30 @@ export const getTraining = async (token: any, date: string) => {
 export const createTraining = async (token: any, date: string) => {
   await connectMongo();
 
+  let totalNumberOfReps = 100;
+  const prevDay = moment(date).subtract(1, "days").format("YYYY-MM-DD");
+  const trainingPrevDay = (await Training.findOne({
+    userId: new ObjectId(token._id),
+    createdAt: prevDay,
+  }).lean()) as TrainingSchemaType;
+
+  if (trainingPrevDay) {
+    const pushUps = 100 - trainingPrevDay.pushUps;
+    const sitUps = 100 - trainingPrevDay.sitUps;
+    const crunches = 100 - trainingPrevDay.crunches;
+
+    const totalNumberOfRepsPrevDay = pushUps + sitUps + crunches;
+    
+    if (totalNumberOfRepsPrevDay > 0) {
+      totalNumberOfReps += totalNumberOfRepsPrevDay;
+    }
+  }
   const training = await Training.create({
     userId: new ObjectId(token._id),
     pushUps: 0,
     sitUps: 0,
     crunches: 0,
-    totalNumberOfReps: 100,
+    totalNumberOfReps,
     createdAt: date,
   });
   return JSON.parse(JSON.stringify(training));
