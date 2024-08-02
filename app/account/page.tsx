287ccbc5dbdbe082f21d9f04ca3c0c5c31dabf63
StroverIT <StroverIT>
2024-08-02 18:08:10 +0300

    Getting timer

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,16 +2,18 @@ import { getServerSession } from "next-auth";
 import options from "@/api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 import Index from "@/components/pages/account/Index";
+import { getTimer } from "@/API/server/timer";
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
   //@ts-ignore
   const session = await getServerSession(options);
+  const timer = await getTimer();
 
   if (!session) {
     redirect("/");
   }
 
   //@ts-ignore
-  return <Index token={session?.token} />;
+  return <Index token={session?.token} timer={timer} />;
 }
