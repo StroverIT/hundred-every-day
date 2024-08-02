6d36a56866687ea1e7e5214b22ca043064b3122e
StroverIT <StroverIT>
2024-08-02 18:08:02 +0300

    Getting timer

---
+++ API/server/timer.ts
@@ -0,0 +1,12 @@
+import options from "@/api/auth/[...nextauth]/options";
+import Timer from "@/db/models/Timer";
+import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+
+export const getTimer = async () => {
+  //@ts-ignore
+  const session = await getServerSession(options);
+  //@ts-ignore
+  const { _id } = session.token;
+  return Timer.findOne({userId: new ObjectId(_id)}).select("-_id -userId -__v").lean();
+};
