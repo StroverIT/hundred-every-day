f39aa2f260d56dc65eff21ee5e0912bbaeef42af
StroverIT <StroverIT>
2024-08-04 01:09:57 +0300

    Add offline close

--- public/scripts/training-worker-notification.js
+++ public/scripts/training-worker-notification.js
@@ -1,19 +1,46 @@
-self.addEventListener("push", (event) => {
-    console.log("push event", event);
-    const data = event.data.json();
-    const title = data.title;
-    const body = data.body;
-    const icon = data.icon;
-  
-    const notificationOptions = {
-      body: body,
-      tag: "training-notification", // Use a unique tag to prevent duplicate notifications
-      icon: icon,
-      data: {
-        url: "/account", // Replace with the desired URL for redirecting user to the desired page
-      },
-    };
-  
-    self.registration.showNotification(title, notificationOptions);
+self.addEventListener('install', () => {
+  console.log('Service worker installed');
 });
-  
+
+self.addEventListener('activate', () => {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('push', event => {
+  const data = event.data.json();
+  const { title, body } = data;
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      // Add more properties as needed
+    })
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close(); // Close the notification
+  // Open the desired URL
+  event.waitUntil(
+    clients.openWindow(process.env.NEXT_PUBLIC_HOSTNAME)
+  );
+});
+
+const cacheName = 'v1';
+
+const cacheClone = async (e) => {
+  const res = await fetch(e.request);
+  const resClone = res.clone();
+
+  const cache = await caches.open(cacheName);
+  await cache.put(e.request, resClone);
+  return res;
+};
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    cacheClone(e)
+      .catch(() => caches.match(e.request))
+      .then((res) => res)
+  );
+});
