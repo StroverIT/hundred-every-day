9ca99c1400ac2885d1172948adb7bbc2b10f1495
StroverIT <StroverIT>
2024-08-04 01:47:16 +0300

    Formatting and adding ulr in data

--- public/scripts/training-worker-notification.js
+++ public/scripts/training-worker-notification.js
@@ -1,32 +1,33 @@
-self.addEventListener('install', () => {
-  console.log('Service worker installed');
+self.addEventListener("install", () => {
+  console.log("Service worker installed");
 });
 
-self.addEventListener('activate', () => {
-  console.log('Service worker activated');
+self.addEventListener("activate", () => {
+  console.log("Service worker activated");
 });
 
-self.addEventListener('push', event => {
+self.addEventListener("push", (event) => {
   const data = event.data.json();
   const { title, body } = data;
 
   event.waitUntil(
     self.registration.showNotification(title, {
       body: body,
+      data: {
+        url: "/account", // Replace with the desired URL for redirecting user to the desired page
+      },
       // Add more properties as needed
     })
   );
 });
 
-self.addEventListener('notificationclick', event => {
+self.addEventListener("notificationclick", (event) => {
   event.notification.close(); // Close the notification
   // Open the desired URL
-  event.waitUntil(
-    clients.openWindow(process.env.NEXT_PUBLIC_HOSTNAME)
-  );
+  event.waitUntil(clients.openWindow(process.env.NEXT_PUBLIC_HOSTNAME));
 });
 
-const cacheName = 'v1';
+const cacheName = "v1";
 
 const cacheClone = async (e) => {
   const res = await fetch(e.request);
@@ -37,7 +38,7 @@ const cacheClone = async (e) => {
   return res;
 };
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     cacheClone(e)
       .catch(() => caches.match(e.request))
