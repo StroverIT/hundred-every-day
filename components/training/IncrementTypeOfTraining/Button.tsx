16daf56ec1c687ef1cb07d66def745da698e3b35
StroverIT <StroverIT>
2024-08-05 23:36:39 +0300

    Adding loading on click

--- components/training/IncrementTypeOfTraining/Button.tsx
+++ components/training/IncrementTypeOfTraining/Button.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { IncrementTypeOfTrainingButton } from "./types";
 
 export default function Button({
   handler,
   value,
 }: IncrementTypeOfTrainingButton) {
-  return <button className="py-4 bg-green-500 text-2xl font-semibold" onClick={() => handler(value)}>{value} +</button>;
+  const [loading, setLoading] = useState(false);
+
+  const insideHandlerForLoading = async () => {
+    setLoading(true);
+    await handler(value);
+    setLoading(false);
+  };
+  return (
+    <button
+      className="py-4 bg-green-500 text-2xl font-semibold"
+      onClick={insideHandlerForLoading}
+    >
+      {loading ? "Loading..." : `+${value}`}
+    </button>
+  );
 }
