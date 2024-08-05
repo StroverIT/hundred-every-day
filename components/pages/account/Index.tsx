2b7b84a5164595d391e3381b322db0e707ba769c
StroverIT <StroverIT>
2024-08-05 08:38:23 +0300

    Removing sending notification from Index

--- components/pages/account/Index.tsx
+++ components/pages/account/Index.tsx
@@ -26,27 +26,6 @@ export default function Index({ token }: IndexProps) {
     initial();
   }, [dateInput]);
 
-  const addTimer = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_HOSTNAME}/api/send-notification`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        token: fcmToken,
-        title: "Test Notification",
-        message: "This is a test notification",
-        link: "/account",
-      }),
-    });
-
-    const data = await response.json();
-  };
-
-  useEffect(() => {
-    addTimer();
-  }, []);
-
   const incrementHandler: IncrementHandlerType = async (
     incrementValue,
     type
